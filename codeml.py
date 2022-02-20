b756893e2ccab0f2ab4c87d23db296a2d791157e
Runjhun Ratawal <runjhunratawal@gmail.com>
2022-02-20 22:20:46 +0530

    Video Capture

--- codeml.py
+++ codeml.py
@@ -3,6 +3,14 @@ import numpy as np
 import pandas as pd
 import cv2 
 
-img_path = ""
-image = cv2.imread(img_path)
-cv2_imshow(image)
+vid = cv2.VideoCapture(0)
+
+while(True):
+	
+	ret, frame = vid.read()
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+vid.release()
+cv2.destroyAllWindows()
